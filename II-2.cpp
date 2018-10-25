4bb7b8d3ba679d514be4055f2ccd4d1cf2e0d2a9
Jonathan Hoff <37785061+haironfyre@users.noreply.github.com>
2018-10-25 18:05:16 -0300

    Create II-2.cpp

---
+++ II-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+int *V, n, maior=0, indice=0;
+
+int *GeraVetor(int tam, int *vetor){
+    vetor=(int*)malloc(tam*sizeof(int));
+    return vetor;
+}
+    
+int *EntraVetor(int tam, int *vetor){
+    for(int j=0; j<tam; j++){
+        cout<<"Digite o valor da posicao: "<<j+1<<": ";
+        cin>>vetor[j];
+        if (vetor[j]<0){
+        	cout<<"Valor invalido. Tente Novamente."<<endl;
+        	j--;
+		}
+    }
+        return vetor;
+}
+
+void MostraVetor(int tam, int *vetor){
+    cout << "Imprimindo o vetor com " << tam << " posicoes" << endl << endl;
+    for(int j=0; j<tam; j++){
+    if (j==0) cout <<"V=[" << vetor[j] << ","; else
+        if(j<n-1) cout << vetor[j] << ","; else
+            cout << vetor[j] << "]" << endl;
+    	}
+	}
+	
+void MaiorMenor(int tam, int *vetor){
+	for (int i=0; i<tam; i++){
+		if(vetor[i]>maior){
+			maior=vetor[i];
+			indice=i;
+		}
+	}
+
+	cout << "O maior eh: "<<maior<<", encotrado no indice: " << indice << endl;
+}
+	
+	
+	
+    
+int main(){
+    cout <<"Qual o tamanho do vetor? ";
+    cin >> n;
+    
+    V =GeraVetor(n,V);
+    V =EntraVetor(n,V);
+    MostraVetor(n,V);
+    MaiorMenor(n,V);
+    free(V);
+    return 0;
+}
